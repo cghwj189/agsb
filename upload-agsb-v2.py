4b6dd1d2879faa42a258e6b581a6efb7224fad87
cghwj189 <95948137+cghwj189@users.noreply.github.com>
2025-06-06 16:58:12 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -34,7 +34,7 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 USER_NAME = "cghwj"         # 用户名
 UUID = "c8f13398-f1b8-4bb3-890d-6b8dafde89e6"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "ping.cloudns.biz"                   # 域名，留空则自动获取
+DOMAIN = "huaming.ping.cloudns.biz"                   # 域名，留空则自动获取
 CF_TOKEN = "eyJhIjoiYWQ3OGRhMzMxNGUyMjQxODlmNjgwYTI1YzlhOTBjNTciLCJ0IjoiM2RiMmRjOGItNTJjNy00NWQzLTk1ZDEtMTlmZDM3MDQxYzY0IiwicyI6Ik0ySTNZbVl5WVdZdE5tUXdOUzAwTm1SaExXRTRaakV0T0RJeVl6VXhZVE0zTVRSayJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
